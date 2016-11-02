dbf598c636ed8086dbaaaa791e5afa5f57de2e2f
Brandon <blshort43@gmail.com>
2016-11-02 18:21:13 -0500

    inital files and list default burgers

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, finished) VALUES ('Big Mac', false);
+INSERT INTO burgers (burger_name, finished) VALUES ('McDouble', false);
+INSERT INTO burgers (burger_name, finished) VALUES ('Cheeseburger', false);
